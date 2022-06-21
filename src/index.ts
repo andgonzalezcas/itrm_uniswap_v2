49e8f171b4c7b3a74e206189880c2e9aa06e8e31
Andres <andgonzalezcas@unal.edu.co>
2022-06-21 14:48:00 -0500

    Se implemento un getMidPrice y se inicio con un getTradePrice

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,63 @@
-import { ChainId, Token, Fetcher } from '@uniswap/sdk'
+import {
+  ChainId,
+  Token,
+  Fetcher,
+  WETH,
+  Pair,
+  Route,
+  Trade,
+  TokenAmount,
+  TradeType
+} from '@uniswap/sdk'
 
-const chainId = ChainId.RINKEBY
-
-let ETH: Token
-let WETH: Token
 let DAI: Token
+let USDC: Token
 
-const getETH = async () => {
-  const tokenAddress = '0x11D634457F99595aBE7B582739fd52b7ed48995A' // ETH token addres
-  ETH = await Fetcher.fetchTokenData(chainId, tokenAddress, undefined, 'ETH', 'Ether')
-  console.log(ETH)
+const daiTokenData = {
+  chainId : ChainId.MAINNET,
+  tokenAddress: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  symbol: 'DAI',
+  name: 'Dai Stablecoin'
 }
 
-const getWETH = async () => {
-  const tokenAddress = '0xDf032Bc4B9dC2782Bb09352007D4C57B75160B15'
-  WETH = await Fetcher.fetchTokenData(chainId, tokenAddress, undefined, 'WETH', 'Wrapped Ether')
-  console.log(WETH)
+const usdcTokenData = {
+  chainId: ChainId.MAINNET,
+  tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+  symbol: 'USDC',
+  name: 'USD Coin'
 }
 
-const getDAI = async () => {
-  const tokenAddress = '0x0165b733e860b1674541BB7409f8a4743A564157'
-  DAI = await Fetcher.fetchTokenData(chainId, tokenAddress, undefined, 'DAI', 'Dai Stablecoin')
-  console.log(DAI)
+// const daiTokenData = {
+//   chainI: ChainId.RINKEBY
+//   tokenAddress: '0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735',
+//   symbol: 'DAI',
+//   name: 'Dai Stablecoin'
+// }
+
+const getToken = async (chainId: number, tokenAddress: string, symbol:string, name:string) => {
+  const token: Token = await Fetcher.fetchTokenData(chainId, tokenAddress, undefined, symbol, name)
+  return token
 }
 
-getETH()
-getWETH()
-getDAI()
+const getPair = async (token1: Token, token2: Token) => {
+  const pair:Pair = await Fetcher.fetchPairData(token1, token2)
+  return pair
+}
+
+const getMidPrice = async (pair: Array<Pair>, baseToken: Token) => {
+  const route = new Route(pair, baseToken)
+  return route
+}
+
+const main = async () => {
+  DAI = await getToken(daiTokenData.chainId, daiTokenData.tokenAddress, daiTokenData.symbol, daiTokenData.name)
+  USDC = await getToken(usdcTokenData.chainId, usdcTokenData.tokenAddress, usdcTokenData.symbol, usdcTokenData.name)
+  const pairDaiUsdc:Pair = await getPair(DAI, USDC)
+  const route:Route = await getMidPrice([pairDaiUsdc], DAI)
+  const trade = new Trade(route, new TokenAmount(DAI, '1000000000000000000'), TradeType.EXACT_INPUT)
+
+  console.log(trade.executionPrice.toSignificant(6))
+  console.log(trade.nextMidPrice.toSignificant(6))
+}
+
+main()
